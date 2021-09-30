c4815a7fa38881c856cec708102cc713796c5cd5
Kamal Mansouri <kamal@eidolonsystems.com>
2021-09-30 00:57:49 -0400

    wip

--- Applications/Scratch/Source/main.cpp
+++ Applications/Scratch/Source/main.cpp
@@ -1,27 +1,124 @@
 #include <cstdint>
 #include <iostream>
 #include <vector>
+#include <stdio.h>
 #include "Version.hpp"
 
+namespace {
+const int BYTES_PER_PIXEL = 3; /// red, green, & blue
+const int FILE_HEADER_SIZE = 14;
+const int INFO_HEADER_SIZE = 40;
+
+unsigned char* createBitmapInfoHeader (int height, int width)
+{
+    static unsigned char infoHeader[] = {
+        0,0,0,0, /// header size
+        0,0,0,0, /// image width
+        0,0,0,0, /// image height
+        0,0,     /// number of color planes
+        0,0,     /// bits per pixel
+        0,0,0,0, /// compression
+        0,0,0,0, /// image size
+        0,0,0,0, /// horizontal resolution
+        0,0,0,0, /// vertical resolution
+        0,0,0,0, /// colors in color table
+        0,0,0,0, /// important color count
+    };
+
+    infoHeader[ 0] = (unsigned char)(INFO_HEADER_SIZE);
+    infoHeader[ 4] = (unsigned char)(width      );
+    infoHeader[ 5] = (unsigned char)(width >>  8);
+    infoHeader[ 6] = (unsigned char)(width >> 16);
+    infoHeader[ 7] = (unsigned char)(width >> 24);
+    infoHeader[ 8] = (unsigned char)(height      );
+    infoHeader[ 9] = (unsigned char)(height >>  8);
+    infoHeader[10] = (unsigned char)(height >> 16);
+    infoHeader[11] = (unsigned char)(height >> 24);
+    infoHeader[12] = (unsigned char)(1);
+    infoHeader[14] = (unsigned char)(BYTES_PER_PIXEL*8);
+
+    return infoHeader;
+}
+
+unsigned char* createBitmapFileHeader (int height, int stride)
+{
+    int fileSize = FILE_HEADER_SIZE + INFO_HEADER_SIZE + (stride * height);
+
+    static unsigned char fileHeader[] = {
+        0,0,     /// signature
+        0,0,0,0, /// image file size in bytes
+        0,0,0,0, /// reserved
+        0,0,0,0, /// start of pixel array
+    };
+
+    fileHeader[ 0] = (unsigned char)('B');
+    fileHeader[ 1] = (unsigned char)('M');
+    fileHeader[ 2] = (unsigned char)(fileSize      );
+    fileHeader[ 3] = (unsigned char)(fileSize >>  8);
+    fileHeader[ 4] = (unsigned char)(fileSize >> 16);
+    fileHeader[ 5] = (unsigned char)(fileSize >> 24);
+    fileHeader[10] = (unsigned char)(FILE_HEADER_SIZE + INFO_HEADER_SIZE);
+
+    return fileHeader;
+}
+
+void generateBitmapImage (const unsigned char* image, int height, int width, const char* imageFileName)
+{
+    int widthInBytes = width * BYTES_PER_PIXEL;
+
+    unsigned char padding[3] = {0, 0, 0};
+    int paddingSize = (4 - (widthInBytes) % 4) % 4;
+
+    int stride = (widthInBytes) + paddingSize;
+
+    FILE* imageFile = fopen(imageFileName, "wb");
+
+    unsigned char* fileHeader = createBitmapFileHeader(height, stride);
+    fwrite(fileHeader, 1, FILE_HEADER_SIZE, imageFile);
+
+    unsigned char* infoHeader = createBitmapInfoHeader(height, width);
+    fwrite(infoHeader, 1, INFO_HEADER_SIZE, imageFile);
+
+    int i;
+    for (i = 0; i < height; i++) {
+        fwrite(image + (i*widthInBytes), BYTES_PER_PIXEL, width, imageFile);
+        fwrite(padding, 1, paddingSize, imageFile);
+    }
+
+    fclose(imageFile);
+}
+}
+
 struct Color {
-  std::uint8_t red;
-  std::uint8_t green;
-  std::uint8_t blue;
-  std::uint8_t alpha;
+  std::uint8_t m_red;
+  std::uint8_t m_green;
+  std::uint8_t m_blue;
+  std::uint8_t m_alpha;
+
+  friend auto operator <=>(const Color&, const Color&) = default;
 };
 
 struct Point {
-  float x;
-  float y;
-  float z;
+  float m_x;
+  float m_y;
+  float m_z;
+
+  friend auto operator <=>(const Point&, const Point&) = default;
 };
 
 std::ostream& operator <<(std::ostream& out, Point point) {
-  return out << "Point(" << point.x << ' ' << point.y << ' ' << point.z << ')';
+  return out <<
+    "Point(" << point.m_x << ' ' << point.m_y << ' ' << point.m_z << ')';
 }
 
 struct Voxel {
-  Color color;
+  constexpr static Voxel NONE() {
+    return Voxel(Color(0, 0, 0, 255));
+  }
+
+  Color m_color;
+
+  friend auto operator <=>(const Voxel&, const Voxel&) = default;
 };
 
 class Model {
@@ -45,7 +142,15 @@ class Cube : public Model {
     }
 
     Voxel get(Point point) const override {
-      return Voxel(m_color);
+      if(point.m_x >= 0 && point.m_y >= 0 && point.m_z >= 0 &&
+          point.m_x < 10 && point.m_y < 10 && point.m_z < m_size) {
+        return Voxel(Color(0, 0, 255, 0));
+      }
+      if(point.m_x >= 0 && point.m_y >= 0 && point.m_z >= 0 &&
+          point.m_x < m_size && point.m_y < m_size && point.m_z < m_size) {
+        return Voxel(m_color);
+      }
+      return Voxel::NONE();
     }
 
   private:
@@ -56,46 +161,72 @@ class Cube : public Model {
 class Scene {
   public:
     Voxel get(Point point) const {
-      return {};
+      return m_models.front()->get(point);
     }
 
-    void add(const Model& model) {}
+    void add(std::shared_ptr<Model> model) {
+      m_models.push_back(std::move(model));
+    }
+
+  private:
+    std::vector<std::shared_ptr<Model>> m_models;
 };
 
 struct Vector {
-  float x;
-  float y;
-  float z;
+  float m_x;
+  float m_y;
+  float m_z;
 };
 
 std::ostream& operator <<(std::ostream& out, Vector vector) {
   return out <<
-    "Vector(" << vector.x << ' ' << vector.y << ' ' << vector.z << ')';
+    "Vector(" << vector.m_x << ' ' << vector.m_y << ' ' << vector.m_z << ')';
 }
 
 Vector operator -(Vector left, Vector right) {
-  return Vector(left.x - right.x, left.y - right.y, left.z - right.z);
+  return Vector(
+    left.m_x - right.m_x, left.m_y - right.m_y, left.m_z - right.m_z);
 }
 
 Vector operator +(Vector left, Vector right) {
-  return Vector(left.x + right.x, left.y + right.y, left.z + right.z);
+  return Vector(
+    left.m_x + right.m_x, left.m_y + right.m_y, left.m_z + right.m_z);
 }
 
 Vector operator *(int left, Vector right) {
-  return Vector(left * right.x, left * right.y, left * right.z);
+  return Vector(left * right.m_x, left * right.m_y, left * right.m_z);
 }
 
 Vector operator *(float left, Vector right) {
-  return Vector(left * right.x, left * right.y, left * right.z);
+  return Vector(left * right.m_x, left * right.m_y, left * right.m_z);
+}
+
+Vector operator /(Vector left, int right) {
+  return Vector(left.m_x / right, left.m_y / right, left.m_z / right);
+}
+
+Vector operator /(Vector left, float right) {
+  return Vector(left.m_x / right, left.m_y / right, left.m_z / right);
 }
 
 Point operator +(Point left, Vector right) {
-  return Point(left.x + right.x, left.y + right.y, left.z + right.z);
+  return Point(
+    left.m_x + right.m_x, left.m_y + right.m_y, left.m_z + right.m_z);
 }
 
 Vector cross(Vector left, Vector right) {
-  return Vector(left.y * right.z - left.z * right.y,
-    left.z * right.x - left.x * right.z, left.x * right.y - left.y * right.x);
+  return Vector(left.m_y * right.m_z - left.m_z * right.m_y,
+    left.m_z * right.m_x - left.m_x * right.m_z,
+    left.m_x * right.m_y - left.m_y * right.m_x);
+}
+
+float dot(Vector left, Vector right) {
+  return left.m_x * right.m_x + left.m_y + right.m_y + left.m_z + right.m_z;
+}
+
+float magnitude(Vector vector) {
+  return std::sqrt(vector.m_x * vector.m_x +
+    vector.m_y * vector.m_y + vector.m_z * vector.m_z);
 }
 
 class Camera {
@@ -132,32 +263,64 @@ class Camera {
 
 std::vector<Color> render(
     const Scene& scene, int width, int height, const Camera& camera) {
-  auto aspect_ratio = static_cast<float>(width) / height;
+  auto pixels = std::vector<Color>();
+  pixels.reserve(width * height);
+  auto aspect_ratio = static_cast<float>(height) / width;
   auto roll = cross(camera.get_orientation(), camera.get_direction());
   auto top_left_direction =
     camera.get_direction() - roll + aspect_ratio * camera.get_orientation();
   auto x_shift = (2.f / width) * roll;
   auto y_shift = (2 * aspect_ratio / height) * camera.get_orientation();
-  for(auto x = 0; x < width; ++x) {
-    for(auto y = 0; y < height; ++y) {
+  for(auto y = 0; y < height; ++y) {
+    for(auto x = 0; x < width; ++x) {
       auto direction = top_left_direction + x * x_shift - y * y_shift;
       auto point = camera.get_position() + direction;
-      if(x == 0) {
-        std::cout << x << " " << y << ": " << point << std::endl;
+      auto increment = direction / magnitude(direction);
+      auto i = 0;
+      while(true) {
+        auto voxel = scene.get(point);
+        if(voxel != Voxel::NONE()) {
+          pixels.push_back(voxel.m_color);
+          break;
+        }
+        ++i;
+        point = point + increment;
+        if(i == 1000) {
+          pixels.push_back(Color(0, 0, 0));
+          break;
+        }
       }
     }
   }
-  return {};
+  return pixels;
 }
 
 int main(int argc, const char** argv) {
   auto scene = Scene();
-  auto cube = Cube(100, Color(255, 0, 0));
+  auto cube = std::make_shared<Cube>(100, Color(255, 0, 0, 0));
   scene.add(cube);
   auto camera = Camera();
-  camera.set_position(Point(-50, -50, -50));
+  camera.set_position(Point(0, 0, -300));
   camera.set_direction(Vector(0, 0, 1));
   camera.set_orientation(Vector(0, 1, 0));
-  auto pixels = render(scene, 100, 100, camera);
+  auto pixels = render(scene, 1920, 1080, camera);
+
+    constexpr int width = 1920;
+    constexpr int height = 1080;
+    auto image = new unsigned char[height][width][BYTES_PER_PIXEL];
+    auto imageFileName = "bitmapImage.bmp";
+
+    int i, j;
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            image[height - i - 1][j][2] = (unsigned char) ( pixels[j + width * i].m_red);             ///red
+            image[height - i - 1][j][1] = (unsigned char) ( pixels[j + width * i].m_green );              ///green
+            image[height - i - 1][j][0] = (unsigned char) ( pixels[j + width * i].m_blue ); ///blue
+        }
+    }
+
+    generateBitmapImage((unsigned char*) image, height, width, imageFileName);
+    printf("Image generated!!");
+
   return 0;
 }
