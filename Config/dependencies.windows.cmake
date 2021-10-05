df80d9dcc431c7e26c28cd3f555c7feb90feb1b2
Kamal Mansouri <kamal@eidolonsystems.com>
2021-10-04 23:56:47 -0400

    wip

---
+++ Config/dependencies.windows.cmake
@@ -0,0 +1,10 @@
+set(BOOST_INCLUDE_PATH
+  "${PROJECT_BINARY_DIR}/Dependencies/boost_1_77_0/include/boost-1_77")
+set(BOOST_DEBUG_PATH "${PROJECT_BINARY_DIR}/Dependencies/boost_1_77_0/lib")
+set(BOOST_OPTIMIZED_PATH "${PROJECT_BINARY_DIR}/Dependencies/boost_1_77_0/lib")
+set(OPENCL_INCLUDE_PATH
+  "${PROJECT_BINARY_DIR}/Dependencies/OpenCL-SDK/external/OpenCL-Headers")
+set(OPENCL_LIBRARY_DEBUG_PATH
+  "${PROJECT_BINARY_DIR}/Dependencies/OpenCL-SDK/install/lib/OpenCL.lib")
+set(OPENCL_LIBRARY_OPTIMIZED_PATH
+  "${PROJECT_BINARY_DIR}/Dependencies/OpenCL-SDK/install/lib/OpenCL.lib")
--- setup.bat
+++ setup.bat
@@ -28,6 +28,40 @@ IF NOT EXIST doctest-2.4.6 (
   )
   DEL /F /Q 2.4.6.zip
 )
+IF NOT EXIST OpenCL-SDK (
+  git clone https://github.com/KhronosGroup/OpenCL-SDK.git
+  PUSHD OpenCL-SDK
+  git submodule init
+  git submodule update
+  mkdir build
+  PUSHD build
+  cmake -A Win32 ..
+  cmake --build . --target INSTALL --config Debug
+  cmake --build . --target INSTALL --config Release
+  POPD
+  POPD
+)
+IF "%NUMBER_OF_PROCESSORS%" == "" (
+  SET BJAM_PROCESSORS=
+) ELSE (
+  SET BJAM_PROCESSORS="-j%NUMBER_OF_PROCESSORS%"
+)
+IF NOT EXIST boost_1_77_0 (
+  wget https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.zip -O boost_1_77_0.zip --no-check-certificate
+  IF !ERRORLEVEL! LEQ 0 (
+    tar -xf boost_1_77_0.zip
+    PUSHD boost_1_77_0
+    PUSHD tools\build
+    CALL bootstrap.bat vc142
+    POPD
+    tools\build\b2 !BJAM_PROCESSORS! --without-context --prefix="!ROOT!\boost_1_77_0" --build-type=complete address-model=32 toolset=msvc-14.2 link=static,shared runtime-link=shared install
+    tools\build\b2 !BJAM_PROCESSORS! --with-context --prefix="!ROOT!\boost_1_77_0" --build-type=complete address-model=32 toolset=msvc-14.2 link=static runtime-link=shared install
+    POPD
+  ) ELSE (
+    SET EXIT_STATUS=1
+  )
+  DEL /F /Q boost_1_77_0.zip
+)
 IF NOT EXIST cache_files (
   MD cache_files
 )
