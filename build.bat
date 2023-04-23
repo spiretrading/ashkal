1540f69022992206ae560b23b407affc3ba86527
Kamal <kamal@eidolonsystems.com>
2023-04-23 13:06:27 -0400

    wip

--- build.bat
+++ build.bat
@@ -46,4 +46,25 @@ IF "!CONFIG!" == "clean" (
   cmake --build "!ROOT!" --target INSTALL --config "!CONFIG!"
   echo !CONFIG! > CMakeFiles\config.txt
 )
+IF NOT EXIST configure.bat (
+  ECHO @ECHO OFF > configure.bat
+  ECHO CALL "%~dp0configure.bat" %%* >> configure.bat
+)
+IF NOT EXIST build.bat (
+  ECHO @ECHO OFF > build.bat
+  ECHO CALL "%~dp0build.bat" %%* >> build.bat
+)
+CALL:build Applications\Scratch %*
+ENDLOCAL
+EXIT /B %ERRORLEVEL%
+
+:build
+IF NOT EXIST "%~1" (
+  MD "%~1"
+)
+PUSHD "%~1"
+CALL "%~dp0%~1\build.bat" -DD="%ROOT%\Dependencies" %~2 %~3 %~4 %~5 %~6 %~7
+POPD
+
+
 ENDLOCAL
--- configure.bat
+++ configure.bat
@@ -118,4 +118,15 @@ IF EXIST "!DIRECTORY!Source" (
 IF "!RUN_CMAKE!" == "1" (
   cmake -S !DIRECTORY! -DD=!DEPENDENCIES!
 )
+CALL:configure Applications\Scratch %*
+ENDLOCAL
+EXIT /B %ERRORLEVEL%
+
+:configure
+IF NOT EXIST "%~1" (
+  MD "%~1"
+)
+PUSHD "%~1"
+CALL "%~dp0%~1\configure.bat" -DD="%ROOT%\Dependencies" %~2 %~3 %~4 %~5 %~6 %~7
+POPD
 ENDLOCAL
