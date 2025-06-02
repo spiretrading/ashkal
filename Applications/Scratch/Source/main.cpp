b356a1f40a1e5d6964192e77e788ca58c07ef653
Kamal Mansouri <kamal@eidolonsystems.com>
2025-06-01 23:06:08 -0400

    Fixed a bug in how cubes were made.

--- Applications/Scratch/Source/main.cpp
+++ Applications/Scratch/Source/main.cpp
@@ -43,59 +43,73 @@ Color lerp(Color a, Color b, float t) {
     std::lerp(a.m_alpha, b.m_alpha, t));
 }
 
-void render(std::vector<std::uint32_t>& frame_buffer, const Camera& camera,
+void render(std::vector<std::uint32_t>& frame_buffer,
+    std::vector<float> depth_buffer, const Camera& camera,
     const std::vector<Vertex>& vertices, const MeshTriangle& triangle,
     const Matrix& transformation, int width, int height) {
   auto& a = vertices[triangle.m_a];
   auto& b = vertices[triangle.m_b];
   auto& c = vertices[triangle.m_c];
-  auto a_position = transform(transformation * a.m_position, camera);
-  auto b_position = transform(transformation * b.m_position, camera);
-  auto c_position = transform(transformation * c.m_position, camera);
-  auto a_screen = project_to_screen(a_position, width, height);
-  auto b_screen = project_to_screen(b_position, width, height);
-  auto c_screen = project_to_screen(c_position, width, height);
-  if(a_screen.first < 0 || b_screen.first < 0 || c_screen.first < 0) {
+  auto camera_a = transform(transformation * a.m_position, camera);
+  auto camera_b = transform(transformation * b.m_position, camera);
+  auto camera_c = transform(transformation * c.m_position, camera);
+  auto screen_a = project_to_screen(camera_a, width, height);
+  auto screen_b = project_to_screen(camera_b, width, height);
+  auto screen_c = project_to_screen(camera_c, width, height);
+  if(screen_a.first < 0 || screen_b.first < 0 || screen_c.first < 0) {
     return;
   }
   auto min_x =
-    std::max(0, std::min({a_screen.first, b_screen.first, c_screen.first}));
+    std::max(0, std::min({screen_a.first, screen_b.first, screen_c.first}));
   auto max_x = std::min(
-    width - 1, std::max({a_screen.first, b_screen.first, c_screen.first}));
+    width - 1, std::max({screen_a.first, screen_b.first, screen_c.first}));
   auto min_y =
-    std::max(0, std::min({a_screen.second, b_screen.second, c_screen.second}));
+    std::max(0, std::min({screen_a.second, screen_b.second, screen_c.second}));
   auto max_y = std::min(
-    height - 1, std::max({a_screen.second, b_screen.second, c_screen.second}));
+    height - 1, std::max({screen_a.second, screen_b.second, screen_c.second}));
+  auto depth_a = -camera_a.m_z;
+  auto depth_b = -camera_b.m_z;
+  auto depth_c = -camera_c.m_z;
   for(auto y = min_y; y <= max_y; ++y) {
     for(auto x = min_x; x <= max_x; ++x) {
       auto p = std::pair(x, y);
-      auto w0 = compute_edge(b_screen, c_screen, p);
-      auto w1 = compute_edge(c_screen, a_screen, p);
-      auto w2 = compute_edge(a_screen, b_screen, p);
+      auto w0 = compute_edge(screen_b, screen_c, p);
+      auto w1 = compute_edge(screen_c, screen_a, p);
+      auto w2 = compute_edge(screen_a, screen_b, p);
       if(w0 >= 0 && w1 >= 0 && w2 >= 0) {
-        auto c01 = lerp(a.m_color, b.m_color, w1 / (w0 + w1 + w2));
-        auto finalC = lerp(c01, c.m_color, w2 / (w0 + w1 + w2));
-        auto pixel = (std::uint32_t(finalC.m_alpha) << 24) |
-                          (std::uint32_t(finalC.m_red)   << 16) |
-                          (std::uint32_t(finalC.m_green) << 8 ) |
-                          (std::uint32_t(finalC.m_blue));
-        frame_buffer[y * width + x] = pixel;
+        auto sum = w0 + w1 + w2;
+        auto z_interpolated =
+          (w0 * depth_a + w1 * depth_b + w2 * depth_c) / sum;
+        auto index = y * width + x;
+        if(z_interpolated < depth_buffer[index]) {
+          depth_buffer[index] = z_interpolated;
+          auto color = lerp(a.m_color, b.m_color, w1 / sum);
+          auto final_color = lerp(color, c.m_color, w2 / sum);
+          auto pixel =
+            (std::uint32_t(final_color.m_alpha) << 24) |
+            (std::uint32_t(final_color.m_red)   << 16) |
+            (std::uint32_t(final_color.m_green) << 8 ) |
+            (std::uint32_t(final_color.m_blue));
+          frame_buffer[index] = pixel;
+        }
       }
     }
   }
 }
 
-void render(std::vector<std::uint32_t>& frame_buffer, const Camera& camera,
+void render(std::vector<std::uint32_t>& frame_buffer,
+    std::vector<float>& depth_buffer, const Camera& camera,
     const std::vector<Vertex>& vertices,
     const std::vector<MeshTriangle>& triangles, const Matrix& transformation,
     int width, int height) {
   for(auto& triangle : triangles) {
-    render(frame_buffer, camera, vertices, triangle, transformation, width,
-      height);
+    render(frame_buffer, depth_buffer, camera, vertices, triangle,
+      transformation, width, height);
   }
 }
 
-void render(std::vector<std::uint32_t>& frame_buffer, const Camera& camera,
+void render(std::vector<std::uint32_t>& frame_buffer,
+    std::vector<float>& depth_buffer, const Camera& camera,
     const std::vector<Vertex>& vertices, const MeshNode& node,
     const Transformation& transformation, const Matrix& parent_transformation,
     int width, int height) {
@@ -103,26 +117,27 @@ void render(std::vector<std::uint32_t>& frame_buffer, const Camera& camera,
     parent_transformation * transformation.get_transformation(node);
   if(node.get_type() == MeshNode::Type::CHUNK) {
     for(auto& child : node.as_chunk()) {
-      render(frame_buffer, camera, vertices, child, transformation,
-        next_transformation, width, height);
+      render(frame_buffer, depth_buffer, camera, vertices, child,
+        transformation, next_transformation, width, height);
     }
   } else {
-    render(frame_buffer, camera, vertices, node.as_triangles(),
+    render(frame_buffer, depth_buffer, camera, vertices, node.as_triangles(),
       next_transformation, width, height);
   }
 }
 
-void render(std::vector<std::uint32_t>& frame_buffer, const Camera& camera,
-    const Mesh& mesh, const Transformation& transformation, int width,
-    int height) {
-  render(frame_buffer, camera, mesh.m_vertices, mesh.m_root, transformation,
-    Matrix::IDENTITY(), width, height);
+void render(std::vector<std::uint32_t>& frame_buffer,
+    std::vector<float>& depth_buffer, const Camera& camera, const Mesh& mesh,
+    const Transformation& transformation, int width, int height) {
+  render(frame_buffer, depth_buffer, camera, mesh.m_vertices, mesh.m_root,
+    transformation, Matrix::IDENTITY(), width, height);
 }
 
-void render(std::vector<std::uint32_t>& frame_buffer, const Camera& camera,
+void render(std::vector<std::uint32_t>& frame_buffer,
+    std::vector<float>& depth_buffer, const Camera& camera,
     const SceneElement& element, int width, int height) {
-  render(frame_buffer, camera, element.get_mesh(), element.get_transformation(),
-    width, height);
+  render(frame_buffer, depth_buffer, camera, element.get_mesh(),
+    element.get_transformation(), width, height);
 }
 
 Mesh make_cube() {
@@ -138,75 +153,42 @@ Mesh make_cube() {
   auto red = Color(255, 0, 0, 255);
   auto green = Color(0, 255, 0, 255);
   auto blue = Color(0, 0, 255, 255);
-
-  // +X face (normal = {1,0,0})
-  //  (1, -1, -1), (1, 1, -1), (1, 1, 1), (1, -1, 1)
-  addVertex(1, -1, -1, 1, 0, 0, white); // idx  0
-  addVertex(1, 1, -1, 1, 0, 0, red); // idx  1
-  addVertex(1, 1, 1, 1, 0, 0, green); // idx  2
-  addVertex(1, -1, 1, 1, 0, 0, blue); // idx  3
-
-  // -X face (normal = {-1,0,0})
-  //  (-1, -1,  1), (-1,  1,  1), (-1,  1, -1), (-1, -1, -1)
-  addVertex(-1, -1, 1, -1, 0, 0, blue); // idx  4
-  addVertex(-1, 1, 1, -1, 0, 0, red); // idx  5
-  addVertex(-1, 1, -1, -1, 0, 0, green); // idx  6
-  addVertex(-1, -1, -1, -1, 0, 0, white); // idx  7
-
-  // +Y face (normal = {0,1,0})
-  //  (-1, 1, -1), (1, 1, -1), (1, 1, 1), (-1, 1, 1)
-  addVertex(-1, 1, -1, 0, 1, 0, red); // idx  8
-  addVertex( 1, 1, -1, 0, 1, 0, blue); // idx  9
-  addVertex( 1, 1, 1, 0, 1, 0, white); // idx 10
-  addVertex(-1, 1, 1, 0, 1, 0, green); // idx 11
-
-  // -Y face (normal = {0,-1,0})
-  //  (-1, -1,  1), (1, -1,  1), (1, -1, -1), (-1, -1, -1)
-  addVertex(-1, -1, 1, 0,-1, 0, red); // idx 12
-  addVertex( 1, -1, 1, 0,-1, 0, green); // idx 13
-  addVertex( 1, -1, -1, 0,-1, 0, blue); // idx 14
-  addVertex(-1, -1, -1, 0,-1, 0, white); // idx 15
-
-  // +Z face (normal = {0,0,1})
-  //  (-1, -1, 1), (1, -1, 1), (1, 1, 1), (-1, 1, 1)
-  addVertex(-1, -1, 1, 0, 0, 1, blue); // idx 16
-  addVertex( 1, -1, 1, 0, 0, 1, green); // idx 17
-  addVertex( 1,  1, 1, 0, 0, 1, white); // idx 18
-  addVertex(-1,  1, 1, 0, 0, 1, red); // idx 19
-
-  // -Z face (normal = {0,0,-1})
-  //  (1, -1, -1), (-1, -1, -1), (-1, 1, -1), (1, 1, -1)
-  addVertex( 1, -1, -1, 0, 0,-1, blue); // idx 20
-  addVertex(-1, -1, -1, 0, 0,-1, red); // idx 21
-  addVertex(-1,  1, -1, 0, 0,-1, green); // idx 22
-  addVertex( 1,  1, -1, 0, 0,-1, white); // idx 23
-
-    // Now add 12 triangles (two per face), using the indices above:
-
-  // +X face: (0,1,2), (0,2,3)
+  addVertex(1, -1, -1, 1, 0, 0, white);
+  addVertex(1, 1, -1, 1, 0, 0, red);
+  addVertex(1, 1, 1, 1, 0, 0, green);
+  addVertex(1, -1, 1, 1, 0, 0, blue);
+  addVertex(-1, -1, 1, -1, 0, 0, blue);
+  addVertex(-1, 1, 1, -1, 0, 0, red);
+  addVertex(-1, 1, -1, -1, 0, 0, green);
+  addVertex(-1, -1, -1, -1, 0, 0, white);
+  addVertex(-1, 1, -1, 0, 1, 0, red);
+  addVertex(-1, 1, 1, 0, 1, 0, green);
+  addVertex(1, 1, 1, 0, 1, 0, white);
+  addVertex(1, 1, -1, 0, 1, 0, blue);
+  addVertex(-1, -1, 1, 0,-1, 0, red);
+  addVertex(1, -1, 1, 0,-1, 0, green);
+  addVertex(1, -1, -1, 0,-1, 0, blue);
+  addVertex(-1, -1, -1, 0,-1, 0, white);
+  addVertex(-1, -1, 1, 0, 0, 1, blue);
+  addVertex(1, -1, 1, 0, 0, 1, green);
+  addVertex(1, 1, 1, 0, 0, 1, white);
+  addVertex(-1, 1, 1, 0, 0, 1, red);
+  addVertex(1, -1, -1, 0, 0,-1, blue);
+  addVertex(-1, -1, -1, 0, 0,-1, red);
+  addVertex(-1, 1, -1, 0, 0,-1, green);
+  addVertex(1, 1, -1, 0, 0,-1, white);
   triangles.push_back({0, 1, 2});
   triangles.push_back({0, 2, 3});
-
-  // -X face: (4,5,6), (4,6,7)
   triangles.push_back({4, 5, 6});
   triangles.push_back({4, 6, 7});
-
-  // +Y face: ( 8,  9, 10), ( 8, 10, 11)
   triangles.push_back({8, 9, 10});
   triangles.push_back({8, 10, 11});
-
-  // -Y face: (12, 13, 14), (12, 14, 15)
-  triangles.push_back({12, 13, 14});
-  triangles.push_back({12, 14, 15});
-
-  // +Z face: (16, 17, 18), (16, 18, 19)
+  triangles.push_back({12, 14, 13});
+  triangles.push_back({12, 15, 14});
   triangles.push_back({16, 17, 18});
   triangles.push_back({16, 18, 19});
-
-  // -Z face: (20, 21, 22), (20, 22, 23)
   triangles.push_back({20, 21, 22});
   triangles.push_back({20, 22, 23});
-
   return Mesh(std::move(vertices), MeshNode(std::move(triangles)));
 }
 
@@ -221,6 +203,8 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
   auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
   auto frame_buffer = std::vector<std::uint32_t>(WIDTH * HEIGHT, 0);
+  auto depth_buffer =
+    std::vector<float>(WIDTH * HEIGHT, std::numeric_limits<float>::infinity());
   auto camera = Camera(Point(0, 0, 10), Vector(0, 0, -1), Vector(0, 1, 0));
   auto cube = SceneElement(make_cube());
   auto is_running = true;
@@ -231,22 +215,16 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
   auto last_mouse_x = std::numeric_limits<int>::min();
   auto last_mouse_y = std::numeric_limits<int>::min();
   while(is_running) {
-    std::fill(frame_buffer.begin(), frame_buffer.end(), 0x00000000);
-    if(SDL_PollEvent(&event)) {
-      switch(event.type) {
-        case SDL_WINDOWEVENT:
-          if(event.window.windowID == window_id) {
-            switch(event.window.event) {
-              case SDL_WINDOWEVENT_CLOSE:
-                event.type = SDL_QUIT;
-                SDL_PushEvent(&event);
-                break;
-            }
-          }
-          break;
-        case SDL_QUIT:
-          is_running = false;
-          break;
+    std::fill(frame_buffer.begin(), frame_buffer.end(), 0);
+    std::fill(depth_buffer.begin(), depth_buffer.end(),
+      std::numeric_limits<float>::infinity());
+    while(SDL_PollEvent(&event)) {
+      if(event.type == SDL_WINDOWEVENT && event.window.windowID == window_id &&
+          event.window.event == SDL_WINDOWEVENT_CLOSE) {
+        event.type = SDL_QUIT;
+        SDL_PushEvent(&event);
+      } else if(event.type == SDL_QUIT) {
+        is_running = false;
       }
     }
     auto state = SDL_GetKeyboardState(nullptr);
@@ -273,17 +251,16 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     }
     last_mouse_x = mouse_x;
     last_mouse_y = mouse_y;
-//    cube.get_transformation().apply(roll(0.1), cube.get_mesh().m_root);
+    cube.get_transformation().apply(roll(0.1), cube.get_mesh().m_root);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    render(frame_buffer, camera, cube, WIDTH, HEIGHT);
+    render(frame_buffer, depth_buffer, camera, cube, WIDTH, HEIGHT);
     SDL_UpdateTexture(
       texture, nullptr, frame_buffer.data(), WIDTH * sizeof(std::uint32_t));
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, texture, nullptr, nullptr);
     SDL_RenderPresent(renderer);
-    SDL_Delay(32);
   }
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
